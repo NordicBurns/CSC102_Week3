05a09f9bd2d932a0b7c9687bff6cf228ae512f3f
NordicBurns <90578429+NordicBurns@users.noreply.github.com>
2021-09-28 00:16:19 -0700

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,67 @@
+function playCraps() {
+    console.log("playCraps() started");
+    var die1 = Math.ceil(Math.random( ) * 6);
+    var die2 = Math.ceil(Math.random( ) * 6);
+    var sum = die1 + die2;
+    document.getElementById("die1Res").innerHTML = die1;
+    document.getElementById("die2Res").innerHTML = die2;
+    document.getElementById("sum1Res").innerHTML = sum;
+    if (sum == 7 || sum == 11) {
+        document.getElementById("finalRes").innerHTML =
+            "Craps - You Lose";
+    }
+    else if (die1 == die2 && die1 % 2 == 0) {
+        document.getElementById("finalRes").innerHTML =
+            "Doubles - You Win";
+    } else {
+        document.getElementById("finalRes").innerHTML =
+            "Draw - Try Again";
+    }
+}
+
+
+function Blastoff() {
+    var currTime = 50;
+    document.getElementById("CountdownTimer").innerHTML = currTime + (str.fontcolor("aqua"));
+    document.getElementById("CountdownTimer").innerHTML = currTime;
+    setTimeout(function () {
+        currTime = currTime - 5;
+        document.getElementById("CountdownTimer").innerHTML = currTime;
+    }, 5000);
+    setTimeout(function () {
+        currTime = currTime - 5;
+        document.getElementById("CountdownTimer").innerHTML = currTime;
+    }, 10000);
+    setTimeout(function () {
+        currTime = currTime - 5;
+        document.getElementById("CountdownTimer").innerHTML = currTime;
+    }, 15000);
+    setTimeout(function () {
+        currTime = currTime - 5;
+        document.getElementById("CountdownTimer").innerHTML = currTime;
+    }, 20000);
+    setTimeout(function () {
+        currTime = currTime - 5;
+        document.getElementById("CountdownTimer").innerHTML = currTime;
+    }, 25000);
+    setTimeout(function () {
+        currTime = currTime - 5;
+        document.getElementById("CountdownTimer").innerHTML = currTime;
+    }, 30000);
+    setTimeout(function () {
+        currTime = currTime - 5;
+        document.getElementById("CountdownTimer").innerHTML = currTime;
+    }, 35000);
+    setTimeout(function () {
+        currTime = currTime - 5;
+        document.getElementById("CountdownTimer").innerHTML = currTime;
+    }, 40000);
+    setTimeout(function () {
+        currTime = currTime - 5;
+        document.getElementById("CountdownTimer").innerHTML = currTime;
+    }, 45000);
+    setTimeout(function () {
+        currTime = currTime - 5;
+        document.getElementById("CountdownTimer").innerHTML = "Blastoff!!!";
+    }, 50000);
+}
