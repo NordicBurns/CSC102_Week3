86235bb1c4bf2029e157fc4ef1c6eadde34301ac
NordicBurns <90578429+NordicBurns@users.noreply.github.com>
2021-10-06 10:14:33 -0700

    Code + Comments

--- Code.js
+++ Code.js
@@ -1,67 +1,66 @@
-function playCraps() {
-    console.log("playCraps() started");
-    var die1 = Math.ceil(Math.random( ) * 6);
-    var die2 = Math.ceil(Math.random( ) * 6);
-    var sum = die1 + die2;
-    document.getElementById("die1Res").innerHTML = die1;
-    document.getElementById("die2Res").innerHTML = die2;
-    document.getElementById("sum1Res").innerHTML = sum;
-    if (sum == 7 || sum == 11) {
-        document.getElementById("finalRes").innerHTML =
-            "Craps - You Lose";
+function playCraps() { // Function to play craps
+    console.log("playCraps() started"); // Links to HTML code 
+    var die1 = Math.ceil(Math.random( ) * 6); // Die one max is 6
+    var die2 = Math.ceil(Math.random( ) * 6); // Die two's max is 6
+    var sum = die1 + die2; // The sum equals Die 1 + Die 2
+    document.getElementById("die1Res").innerHTML = die1; //Gets die one by thw element's ID
+    document.getElementById("die2Res").innerHTML = die2; // Gets die two by the element's ID
+    document.getElementById("sum1Res").innerHTML = sum; // Writes the sum by the element's ID
+    if (sum == 7 || sum == 11) { // If the sum is 7 or 11
+        document.getElementById("finalRes").innerHTML = // Gets final res by ID
+            "Craps - You Lose"; // YOU LOSE
     }
-    else if (die1 == die2 && die1 % 2 == 0) {
-        document.getElementById("finalRes").innerHTML =
-            "Doubles - You Win";
-    } else {
-        document.getElementById("finalRes").innerHTML =
-            "Draw - Try Again";
+    else if (die1 == die2 && die1 % 2 == 0) { // If die one equals die two 
+        document.getElementById("finalRes").innerHTML = // Gets final res by ID
+            "Doubles - You Win"; // YOU WIN
+    } else { // Otherwise
+        document.getElementById("finalRes").innerHTML = // Gets final res by ID
+            "Draw - Try Again"; // DRAW
     }
 }
 
 
-function Blastoff() {
-    var currTime = 50;
-    document.getElementById("CountdownTimer").innerHTML = currTime + (str.fontcolor("aqua"));
-    document.getElementById("CountdownTimer").innerHTML = currTime;
-    setTimeout(function () {
-        currTime = currTime - 5;
-        document.getElementById("CountdownTimer").innerHTML = currTime;
-    }, 5000);
-    setTimeout(function () {
-        currTime = currTime - 5;
-        document.getElementById("CountdownTimer").innerHTML = currTime;
-    }, 10000);
-    setTimeout(function () {
-        currTime = currTime - 5;
-        document.getElementById("CountdownTimer").innerHTML = currTime;
-    }, 15000);
-    setTimeout(function () {
-        currTime = currTime - 5;
-        document.getElementById("CountdownTimer").innerHTML = currTime;
-    }, 20000);
-    setTimeout(function () {
-        currTime = currTime - 5;
-        document.getElementById("CountdownTimer").innerHTML = currTime;
-    }, 25000);
-    setTimeout(function () {
-        currTime = currTime - 5;
-        document.getElementById("CountdownTimer").innerHTML = currTime;
-    }, 30000);
-    setTimeout(function () {
-        currTime = currTime - 5;
-        document.getElementById("CountdownTimer").innerHTML = currTime;
-    }, 35000);
-    setTimeout(function () {
-        currTime = currTime - 5;
-        document.getElementById("CountdownTimer").innerHTML = currTime;
-    }, 40000);
-    setTimeout(function () {
-        currTime = currTime - 5;
-        document.getElementById("CountdownTimer").innerHTML = currTime;
-    }, 45000);
-    setTimeout(function () {
-        currTime = currTime - 5;
-        document.getElementById("CountdownTimer").innerHTML = "Blastoff!!!";
-    }, 50000);
+function Blastoff() { // Timeloop Function
+    var currTime = 50; // Amount of Time in TImer,50sec
+    document.getElementById("CountdownTimer").innerHTML = currTime; // Sets countdown timer with current time
+    setTimeout(function () { //Set timeout funtction
+        currTime = currTime - 5; // Current time subtracted by 5 seconds
+        document.getElementById("CountdownTimer").innerHTML = currTime; // Gets countdown timer by element's ID + current time
+    }, 5000); // Subtract 5000 milisecends
+    setTimeout(function () {//Set timeout funtction
+        currTime = currTime - 5;// Current time subtracted by 5 seconds
+        document.getElementById("CountdownTimer").innerHTML = currTime;// Gets countdown timer by element's ID + current time
+    }, 10000);// Subtract 10000 milisecends
+    setTimeout(function () {//Set timeout funtction
+        currTime = currTime - 5;// Current time subtracted by 5 seconds
+        document.getElementById("CountdownTimer").innerHTML = currTime;// Gets countdown timer by element's ID + current time
+    }, 15000);// Subtract 15000 milisecends
+    setTimeout(function () {//Set timeout funtction
+        currTime = currTime - 5;// Current time subtracted by 5 seconds
+        document.getElementById("CountdownTimer").innerHTML = currTime;// Gets countdown timer by element's ID + current time
+    }, 20000);// Subtract 20000 milisecends
+    setTimeout(function () {//Set timeout funtction
+        currTime = currTime - 5;// Current time subtracted by 5 seconds
+        document.getElementById("CountdownTimer").innerHTML = currTime;// Gets countdown timer by element's ID + current time
+    }, 25000);// Subtract 25000 milisecends
+    setTimeout(function () {//Set timeout funtction
+        currTime = currTime - 5;// Current time subtracted by 5 seconds
+        document.getElementById("CountdownTimer").innerHTML = currTime;// Gets countdown timer by element's ID + current time
+    }, 30000);// Subtract 30000 milisecends
+    setTimeout(function () {//Set timeout funtction
+        currTime = currTime - 5;// Current time subtracted by 5 seconds
+        document.getElementById("CountdownTimer").innerHTML = currTime;// Gets countdown timer by element's ID + current time
+    }, 35000);// Subtract 35000 milisecends
+    setTimeout(function () {//Set timeout funtction
+        currTime = currTime - 5;// Current time subtracted by 5 seconds
+        document.getElementById("CountdownTimer").innerHTML = currTime;// Gets countdown timer by element's ID + current time
+    }, 40000);// Subtract 40000 milisecends
+    setTimeout(function () {//Set timeout funtction
+        currTime = currTime - 5;// Current time subtracted by 5 seconds
+        document.getElementById("CountdownTimer").innerHTML = currTime;// Gets countdown timer by element's ID + current time
+    }, 45000);// Subtract 45000 milisecends
+    setTimeout(function () {//Set timeout funtction
+        currTime = currTime - 5;// Current time subtracted by 5 seconds
+        document.getElementById("CountdownTimer").innerHTML = "Blastoff!!!";// Gets countdown timer by element's ID + Blastoff!!!
+    }, 50000);// Subtract 50000 milisecends
 }
